9edf00851d9e4e54527baf154968332e49407a3e
Okpeahior Gregory <greghim53lf+github@gmail.com>
2024-07-03 18:35:53 +0100

    updates-8

--- app/(root)/referral/page.tsx
+++ app/(root)/referral/page.tsx
@@ -4,7 +4,7 @@ import PatientReferralForm from "@/forms/PatientReferralForm";
 export default function Referral() {
   return (
     <MaxWidthWrapper className="py-20">
-      <h3 className="text-center sm:text-left text-3xl md:text-5xl font-bold mb-16 text-blue-500 tracking-wide">
+      <h3 className="text-center sm:text-left text-3xl md:text-5xl font-bold mb-16 text-blue-700 tracking-wide">
         Referral Form
       </h3>
       <PatientReferralForm />

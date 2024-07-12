fbae61f5e1dec05890402e3ec8160704dc9ed980
Okpeahior Gregory <greghim53lf+github@gmail.com>
2024-07-12 11:43:30 +0100

    new error update

--- app/admin/referrals/page.tsx
+++ app/admin/referrals/page.tsx
@@ -1,10 +1,10 @@
 import { DataTable } from "@/app/admin/_components/table/DataTable";
 import { referralColumns } from "@/app/admin/_components/table/referralColumns";
 import MaxWidthWrapper from "@/components/shared/MaxWidthWrapper";
-import { getPatientReferrals } from "@/lib/actions/referrals.actions";
+import { getReferrals } from "@/lib/actions/referrals.actions";
 
 export default async function page() {
-  const referrals = await getPatientReferrals();
+  const referrals = await getReferrals();
 
   return (
     <MaxWidthWrapper>
